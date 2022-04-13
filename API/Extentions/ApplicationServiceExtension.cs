130e1766985bd85a1117e5a7ec5d8c72d867b3bb
baodzzzz <quocbao170801@gmail.com>
2022-04-13 17:41:56 +0700

    com

---
+++ API/Extentions/ApplicationServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extentions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DatingAppContext>();
+            return services;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Extentions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -32,25 +33,14 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddDbContext<DatingAppContext>();
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
             services.AddCors();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(option =>
-            {
-                option.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config[("TokenKey")])),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                };
-            });
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
