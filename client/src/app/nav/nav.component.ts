c13d8c2ed1bdc2a939d5a1b4e9ab0ab86a864eca
baodzzzz <97079591+baodzzzz@users.noreply.github.com>
2022-04-25 13:37:12 +0700

    new modules

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +13,8 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
  
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
   currentUser: any;
 
   ngOnInit(): void {
@@ -25,6 +27,8 @@ export class NavComponent implements OnInit {
       this.currentUser = localStorage.getItem('user');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
+      
     });
   }
   logout(){
